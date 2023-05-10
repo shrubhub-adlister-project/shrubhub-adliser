f12e0e1f9703be3d22a71f3ea696c28208089249
Quintyn Williams <t.quin.williams@gmail.com>
2023-05-10 14:19:41 -0500

    Guessing Game with Servlets

--- src/main/java/controllers/GuessServlet.java
+++ src/main/java/controllers/GuessServlet.java
@@ -7,25 +7,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/pickcolor")
-public class ColorServlet extends HttpServlet {
+@WebServlet("/guess")
+public class GuessServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        req.getRequestDispatcher("/forms/pickcolor.jsp").forward( req, resp );
+        req.getRequestDispatcher("/forms/guess.jsp").forward( req, resp );
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        String userColor = req.getParameter("color");
+        String userGuess = req.getParameter("guess");
 
-        req.setAttribute("color", userColor);
-
-        System.out.println("leaving Color");
-
-        req.getRequestDispatcher("/forms/viewcolor.jsp").forward( req, resp );
+        if (userGuess.equals("2")) {
+            resp.sendRedirect("/correct");
+        } else {
+            resp.sendRedirect("/incorrect");
+        }
 
     }
 }
---
+++ src/main/webapp/forms/guess.jsp
@@ -0,0 +1,19 @@
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <title>Guessing Game</title>
+</head>
+<body>
+
+<h3>Guess a number between 1 and 3:</h3>
+<form action="guess" method="POST">
+  <label for="input">
+    <input type="number" id="input" name="guess" min="1" max="3">
+  </label>
+</form>
+
+</body>
+</html>
